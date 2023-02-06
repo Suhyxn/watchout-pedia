d54f872e4cea6406b6d7239c1e80f80826fa5a65
Suhyxn <momentomori21489@gmail.com>
2023-02-06 18:23:16 +0900

    feat: Settings router pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MainPage from "./Pages/MainPage";
+import MovieDetail from "./Pages/MovieDetail";
+import TvDetail from "./Pages/TvDetail";
+import TvPage from "./Pages/TvPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/"/>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/tv" element={<TvPage />} />
+        <Route path="/movie/:id" element={<MovieDetail />} />
+        <Route path="/tv/:id" element={<TvDetail />} />
       </Routes>
     </Router>
   );
