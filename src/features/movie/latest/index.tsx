c1637e2ffb546d71efcb4cd6c4df1ba5524a274c
Suhyxn <momentomori21489@gmail.com>
2023-02-21 05:15:28 +0900

    chore: Install react-query

---
+++ src/features/movie/latest/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Base = styled.div``;
+
+const Title = styled.h4``;
+
+function LatestMovieSection() {
+  return (
+    <Base>
+      <Title>최근 개봉작</Title>
+    </Base>
+  );
+}
+
+export default LatestMovieSection;
