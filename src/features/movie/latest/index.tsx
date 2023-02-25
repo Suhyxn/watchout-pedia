84b92c93d5589cdaff9eeef4c90bb629946553de
Suhyxn <momentomori21489@gmail.com>
2023-02-25 20:33:13 +0900

    feat: Create LatestMovieSection

--- src/features/movie/latest/index.tsx
+++ src/features/movie/latest/index.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import styled from "@emotion/styled";
+import useLatestMovie from "./useLatestMovie";
 
 const Base = styled.div``;
 
 const Title = styled.h4``;
 
 function LatestMovieSection() {
+  const { data, isLoading } = useLatestMovie();
+
   return (
     <Base>
       <Title>최근 개봉작</Title>
+      {isLoading ? <div>Loading...</div> : <div>{data?.data.title}</div>}
     </Base>
   );
 }
